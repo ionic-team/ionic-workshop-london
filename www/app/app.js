1ccd61642273f3ffabcedfcaa4a4d6cac6346385
perry <perry@ionic.io>
2015-10-16 11:07:27 -0500

    config demonstration

--- www/app/app.js
+++ www/app/app.js
@@ -1,4 +1,4 @@
-import {App, IonicApp, IonicPlatform} from 'ionic/ionic';
+import {App, IonicApp, IonicPlatform, IonicConfig} from 'ionic/ionic';
 import {Http, HTTP_BINDINGS} from "angular2/http";
 import {TabsPage} from './tabs/tabs';
 import {Friends} from './data/data';
@@ -7,11 +7,13 @@ import {SessionsService} from './data/sessions';
 
 @App({
   template: `<ion-nav [root]="root"></ion-nav>`,
-  providers: [Friends, SpeakersService, SessionsService, Http, HTTP_BINDINGS]
+  providers: [Friends, SpeakersService, SessionsService, Http, HTTP_BINDINGS],
+  //config: {mode:'md'}
 })
 
 class MyApp {
-  constructor(app: IonicApp, platform: IonicPlatform) {
+  constructor(app: IonicApp, platform: IonicPlatform, config:IonicConfig) {
     this.root = TabsPage;
+    //config.set('mode', 'md');
   }
 }
